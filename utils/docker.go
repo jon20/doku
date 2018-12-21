3bebeb0d0772c65598a2b63f59debdc38b2753a4
jon <kawadu1221@gmail.com>
2018-12-21 10:22:48 +0900

    add get container list function

--- utils/docker.go
+++ utils/docker.go
@@ -1,1 +1,29 @@
 package utils
+
+import (
+	"fmt"
+	"github.com/docker/docker/client"
+	"context"
+	"github.com/docker/docker/api/types"
+)
+
+type docker struct {
+	Client *client.Client
+}
+
+func NewDockerClient(connect *client.Client) docker {
+	return docker{connect}
+}
+
+func (d *docker) GetImageList() error {
+	images, err := d.Client.ImageList(context.Background(), types.ImageListOptions{})
+	if err != nil {
+		return nil
+	}
+	for _, image := range images {
+		fmt.Println(image.ID)
+		fmt.Println(image.RepoTags)
+	}
+	return nil
+}
+
