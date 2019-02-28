680646e508c5f1dd32eeb4c19274a2d957ac993f
jon <kawadu1221@gmail.com>
2019-02-28 18:32:23 +0900

    Change call function place and Change view name

--- ui/view.go
+++ ui/view.go
@@ -9,7 +9,7 @@ import (
 // Image List View creates docker image list components
 func ImageListView(g *gocui.Gui, maxX int, maxY int) error {
 
-	v, err := g.SetView("Image", 0, 0, maxX-1, maxY/2)
+	v, err := g.SetView("Image List", 0, 0, maxX-1, maxY/2)
 	if err != nil && err != gocui.ErrUnknownView {
 		panic(err)
 	}
@@ -17,11 +17,10 @@ func ImageListView(g *gocui.Gui, maxX int, maxY int) error {
 	v.Frame = true
 	v.Title = v.Name()
 	v.FgColor = gocui.AttrBold | gocui.ColorRed
-	v.Clear()
 	line := FormatImageLine(v, "REPOSITORY", "TAG", "IMAGE ID", "SIZE", maxX)
 	fmt.Fprintln(v, line)
 
-	v, err = g.SetView("Image List", 0, 1, maxX-1, maxY/2)
+	v, err = g.SetView("Image", 0, 1, maxX-1, maxY/2)
 	if err != nil && err != gocui.ErrUnknownView {
 		panic(err)
 	}
@@ -30,12 +29,13 @@ func ImageListView(g *gocui.Gui, maxX int, maxY int) error {
 	v.Frame = false
 	v.Wrap = true
 	v.Highlight = true
-	go ShowImageListWithAutoRefresh(g)
-	v.SetOrigin(0, 0)
+	//v.SetOrigin(0, 0)
 	v.SetCursor(0, 0)
+	g.SetCurrentView(v.Name())
 	if _, err = SetCurrentViewOnTop(g, v.Name()); err != nil {
 		return err
 	}
+	go ShowImageListWithAutoRefresh(g)
 	return nil
 
 }
