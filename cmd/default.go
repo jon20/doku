47de6131a849db2ef2bb42baabb053055b656746
jon <kawadu1221@gmail.com>
2019-03-27 03:18:51 +0900

    Add keytab trigger

--- cmd/default.go
+++ cmd/default.go
@@ -46,6 +46,9 @@ func SetKeyBindings(g *gocui.Gui) {
 		log.Panicln(err)
 	}
 
+	if err := g.SetKeybinding("", gocui.KeyTab, gocui.ModNone, ui.NextView); err != nil {
+		log.Panicln(err)
+	}
 }
 
 func layout(g *gocui.Gui) error {
